64206aeff4217fb308bce5dbcd675e6dc94d11db
Sriashika Addala <ashika29@users.noreply.github.com>
2021-05-16 15:33:32 +0530

    Minor changes - correction

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
+import React from 'react';
+import comingSoon from './assets/images/ComingSoon.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ background: 'black' }}>
+        <img src={comingSoon} alt="logo" width={1000} />
       </header>
     </div>
   );

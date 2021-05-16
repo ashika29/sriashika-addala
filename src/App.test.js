c29ccd854e07d7b5a91f3679c686b8ed95ab11d4
Sriashika Addala <ashika29@users.noreply.github.com>
2021-05-16 19:32:32 +0530

    App.js Test file modified

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,6 @@ import App from './App';
 
 test('renders the image in app', () => {
   render(<App />);
-  const imageAltText = screen.getAllByAltText(/logo/i)[0];
-  expect(imageAltText).toHaveAttribute('width');
-  expect(imageAltText).toHaveAttribute('data-test-id', 'coming-soon-image');
+  const imageAltText = screen.getByAltText(/glitch/i);
+  expect(imageAltText).toHaveAttribute('src');
 });

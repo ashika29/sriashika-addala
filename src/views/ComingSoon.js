8db10a81a20dec66437df899e10fab49589b7b35
Sriashika Addala <ashika29@users.noreply.github.com>
2021-05-23 14:15:34 +0530

    ComingSoon separated out from Home

---
+++ src/views/ComingSoon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import comingSoon from '../assets/images/ComingSoon.gif';
+import "../App.css";
+
+function ComingSoon() {
+  return (
+    <div>
+      <header className="App-header" style={{ background: 'black' }}>
+        <img src={comingSoon} data-test-id={'coming-soon-image'} alt="logo" width='50%' />
+      </header>
+    </div>
+  );
+}
+
+export default ComingSoon;

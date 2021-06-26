9856ec74734f207a82bde67bac3e54a69109b7bc
Sriashika Addala <ashika29@users.noreply.github.com>
2021-06-26 21:23:32 +0530

    Prettier set up

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Suspense } from 'react';
-import { Routes } from './routes';
-import { BrowserRouter } from 'react-router-dom';
-import Fallback from './components/Fallback';
-import './App.css';
+import React, { Suspense } from "react";
+import { Routes } from "./routes";
+import { BrowserRouter } from "react-router-dom";
+import Fallback from "./components/Fallback";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Suspense fallback={<Fallback />}>
         <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+          <Routes />
+        </BrowserRouter>
       </Suspense>
     </div>
   );

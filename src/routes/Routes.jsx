ce6d7b1dbe77558e3dafc690084ab2324d90b36b
Sriashika Addala <ashika2k8@gmail.com>
2023-04-23 21:47:11 +0530

    Fixed deployment issues

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Routes as Switch, Route } from 'react-router-dom';
 import Paths from './paths';
 
 function Routes() {
   return (
     <Switch>
-      {Paths.map(({ path, component: Component, exact }, key) => (
-        <Route exact={exact} path={path} component={Component} key={key} />
+      {Paths.map(({ path, component: Component, exact }) => (
+        <Route exact={exact} path={path} component={Component} key={`${path}`} />
       ))}
     </Switch>
   );

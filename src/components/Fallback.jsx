ab5eaa5150a87f06bc7207243adc42f6b280d093
Sriashika Addala <ashika2k8@gmail.com>
2023-04-23 23:10:17 +0530

    Fixed the fallback route

--- src/components/Fallback.jsx
+++ src/components/Fallback.jsx
@@ -4,7 +4,14 @@ import glitchImage from '../assets/images/Glitch.gif';
 export default function Fallback() {
   return (
     <div>
-      <img src={glitchImage} alt="Oops there is a technical glitch" />
+      <img
+        src={glitchImage}
+        alt="Oops there is a technical glitch"
+        style={{
+          width: '-webkit-fill-available',
+          height: '-webkit-fill-available'
+        }}
+      />
     </div>
   );
 }
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -3,13 +3,20 @@ import React from 'react';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes as Switch, Route } from 'react-router-dom';
 import Paths from './paths';
+import Fallback from '../components/Fallback';
 
 function Routes() {
+  const currentPath = window.location.pathname;
+  const whitelistedPaths = Paths.map(({ path }) => path);
+
   return (
     <Switch>
       {Paths.map(({ path, component: Component, exact }) => (
         <Route exact={exact} path={path} element={<Component />} key={`${path}`} />
       ))}
+      {!whitelistedPaths.includes(currentPath) && (
+        <Route exact path={currentPath} element={<Fallback />} />
+      )}
     </Switch>
   );
 }

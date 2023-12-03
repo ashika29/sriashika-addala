d027da75da56ade0812a8a2cbdcb75160290cbd1
Sriashika Addala <sriashikaaddala@gmail.com>
2023-12-03 11:07:41 +0530

    Added basic pg & express nodejs app

--- index.js
+++ index.js
@@ -1,3 +1,51 @@
-const ENVIRONMENT = process.env.ENVIRONMENT;
+import express from "express";
+import cors from "cors";
+import { postgraphile } from "postgraphile";
 
-console.log("hello world: ", ENVIRONMENT);
+const app = express();
+const port = 5001;
+
+app.use(cors());
+app.use(
+    postgraphile(
+        `postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PWD}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+        "public",
+        {
+            disableDefaultMutations: false,
+            dynamicJson: true,
+            graphiql: true,
+            showErrorStack: true,
+            retryOnInitFail: true,
+            extendedErrors: [
+                "severity",
+                "code",
+                "detail",
+                "hint",
+                "position",
+                "internalPosition",
+                "internalQuery",
+                "where",
+                "schema",
+                "table",
+                "column",
+                "dataType",
+                "constraint",
+                "file",
+                "line",
+                "routine",
+            ],
+            enhanceGraphiql: true,
+            allowExplain: (req) => {
+                // eslint-disable-next-line no-console
+                console.info(`Req details: ${req}`);
+                return true;
+            },
+            graphileBuildOptions: {
+                connectionFilterRelations: true, // default: false
+                connectionFilterAllowEmptyObjectInput: true,
+                connectionFilterAllowNullInput: true,
+            },
+        }
+    )
+);
+app.listen(port);

f659c81c8dc1252e3d5b13316ef664171d71462d
Sriashika Addala <sriashikaaddala@gmail.com>
2023-12-09 18:56:26 +0530

    Dummy pg prototype is ready

--- src/graphql/queries/GET_PAGE_CONTENT.ts
+++ src/graphql/queries/GET_PAGE_CONTENT.ts
@@ -1,12 +1,25 @@
 import { gql } from "@apollo/client";
 
-export const GET_PAGE_CONTENT = gql`
+export const GET_PAGES = gql`
   query getPages {
     results: allPages {
       nodes {
         id: pgId
         heading: pgLabel
         path: pgPath
+      }
+    }
+  }
+`;
+
+export const GET_PAGE_CONTENT = gql`
+  query getPageBySlug($slug: String) {
+    results: allVwMetaconfigDummies(condition: { pgPath: $slug }, first: 1) {
+      nodes {
+        id: pgId
+        title: pgLabel
+        slug: pgPath
+        body
       }
     }
   }

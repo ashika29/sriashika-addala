a3b457172739c396eaa157aef1bf03df847c8a88
Sriashika Addala <sriashikaaddala@gmail.com>
2023-12-12 20:49:22 +0530

    Added usePage hook and font wrapper for utility

---
+++ src/utils/customHooks/usePage.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+function usePage(title: string, description?: string): JSX.Element {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}
+
+export default usePage;

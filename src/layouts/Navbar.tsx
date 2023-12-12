386ad24bc326d559ac58f06442d47ab8b66e24ba
Sriashika Addala <sriashikaaddala@gmail.com>
2023-12-12 21:36:12 +0530

    navbar ux enhanced

--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,11 +1,39 @@
-import { useScrollTrigger } from "@mui/material";
+import { Fade, useScrollTrigger } from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const trigger = useScrollTrigger();
+  const [isMouseOver, setIsMouseOver] = useState<boolean>(false);
+  let timer: NodeJS.Timeout;
+
+  useEffect(() => {
+    const handleMouseActivity = () => {
+      setIsMouseOver(true);
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        setIsMouseOver(false);
+      }, 8000);
+    };
+
+    document.addEventListener("mousemove", handleMouseActivity);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseActivity);
+    };
+  }, []);
 
   return (
-    <nav className={trigger ? "navScrolled" : ""}>
-      Navbar &nbsp; &nbsp; Get started by editing src/pages/index.tsx
-    </nav>
+    <Fade
+      in={trigger && isMouseOver}
+      timeout={{
+        appear: 500,
+        enter: 300,
+        exit: 1000,
+      }}
+    >
+      <nav className={trigger && isMouseOver ? "navScrolled" : ""}>
+        Navbar &nbsp; &nbsp; Get started by editing src/pages/index.tsx
+      </nav>
+    </Fade>
   );
 }

287988771cbe5b4a9ebe4607a302905d1b65634d
Sriashika Addala <sriashikaaddala@gmail.com>
2023-12-14 12:14:12 +0530

    Added support for SSR - styles and caching

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,30 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@mui/styles";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentProps,
+  DocumentContext,
+} from "next/document";
+import { AppType } from "next/app";
+import {
+  DocumentHeadTags,
+  DocumentHeadTagsProps,
+} from "@mui/material-nextjs/v13-pagesRouter"; // or `v14-pagesRouter` if you are using Next.js v14
+import { Children } from "react";
+import { MyAppProps } from "@/types/Providers";
+import createEmotionCache from "@/utils/createEmotionCache";
+import createEmotionServer from "@emotion/server/create-instance";
 
-export default function Document() {
+export default function MyDocument(
+  props: DocumentProps & DocumentHeadTagsProps
+) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <DocumentHeadTags {...props} />
+      </Head>
       <body>
         <Main />
         <NextScript />
@@ -11,3 +32,41 @@ export default function Document() {
     </Html>
   );
 }
+
+MyDocument.getInitialProps = async (ctx: DocumentContext) => {
+  // Render app and page and get the context of the page with collected side effects.
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp:
+        (
+          App: React.ComponentType<React.ComponentProps<AppType> & MyAppProps>
+        ) =>
+        (props) =>
+          sheets.collect(<App emotionCache={cache} {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+  const emotionStyles = extractCriticalToChunks(initialProps.html);
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(" ")}`}
+      key={style.key}
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ));
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+    emotionStyleTags,
+  };
+};

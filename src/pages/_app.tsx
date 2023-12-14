db32e3e4c3bdc39a05195d0f947e7200ecb156c6
Sriashika Addala <sriashikaaddala@gmail.com>
2023-12-14 12:14:12 +0530

    Added support for SSR - styles and caching

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,29 @@
 import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import Layout from "./layout";
+import { AppCacheProvider } from "@mui/material-nextjs/v13-pagesRouter";
+import { MyAppProps } from "@/types/Providers";
+import createEmotionCache from "@/utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+import { ThemeProvider, createTheme } from "@mui/material";
+import { SnackbarProvider } from "notistack";
 
-export default function App({ Component, pageProps }: AppProps) {
+const theme = createTheme();
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App(props: MyAppProps) {
+  const { Component, pageProps, emotionCache = clientSideEmotionCache } = props;
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <AppCacheProvider {...props}>
+      <CacheProvider value={emotionCache}>
+        <ThemeProvider theme={theme}>
+          <SnackbarProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </SnackbarProvider>
+        </ThemeProvider>
+      </CacheProvider>
+    </AppCacheProvider>
   );
 }

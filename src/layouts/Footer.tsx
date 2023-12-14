fc15c888a80c87b7f245a8bcbdfb6c0e90df9e14
Sriashika Addala <sriashikaaddala@gmail.com>
2023-12-14 12:13:37 +0530

    Navbar and footer revamped

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,3 +1,31 @@
+import { Grid, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+import config from "@/configs/footer.config.json";
+
 export default function Footer() {
-  return <div>Footer</div>;
+  const classes = useStyles();
+
+  return (
+    <Grid container className={classes.centerAligned}>
+      <Grid item xs={12}>
+        <Typography variant="h3" color="honeydew">
+          {config.mainline}
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Typography variant="body1" color="GrayText">
+          {config.captionText}
+        </Typography>
+      </Grid>
+    </Grid>
+  );
 }
+
+const useStyles = makeStyles({
+  centerAligned: {
+    justifyContent: "center",
+    alignItems: "center",
+    textAlign: "center",
+    marginTop: "6rem",
+  },
+});
